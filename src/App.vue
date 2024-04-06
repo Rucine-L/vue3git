5e723aad240326842ec8df6a4ac414a7e48db059
Rucine-L <438272297@qq.com>
2024-04-06 22:21:33 +0800

    Initial commit of gh-pages branch with built static files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <d3-pie-chart></d3-pie-chart>
+  </div>
+</template>
+
+<script>
+import D3PieChart from './components/D3PieChart.vue';
+
+export default {
+  components: {
+    D3PieChart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
